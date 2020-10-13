a89565222857a01ae7fb7ad164c570388e4f213b
Sergio <sdsoria@gmail.com>
2020-10-13 12:01:58 -0300

    hola mundo y Lista de deseos
    hola mundo y Lista de deseos

--- angular-listadeseos/src/app/app.component.ts
+++ angular-listadeseos/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Curso de Angular';
+  title = 'angular-listadeseos';
+
+  agregar(titulo: HTMLInputElement)	{
+    console.log(titulo);
+    }
 }
--- angular-whishlist/src/app/app.module.ts
+++ angular-whishlist/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SaludadorComponent } from './saludador/saludador.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SaludadorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
